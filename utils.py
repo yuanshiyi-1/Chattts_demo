3c30a9949017b1204d015941b305b758353d08a0
Shiyi Yuan <shiyuan@redhat.com>
2024-06-04 10:18:32 +0800

    update utils.py

--- utils.py
+++ utils.py
@@ -90,8 +90,8 @@ def remove_chinese_punctuation(text):
     pattern = r'\[(.*?)\]'
     matches = re.findall(pattern, text)
     for match in matches:
-	if match != "uv_break" and match != "laugh" and match != "lbreak" and match != "cry":
-		text = text.replace("[" + match + "]", match)
+        if match != "uv_break" and match != "laugh" and match != "lbreak" and match != "cry":
+            text = text.replace("[" + match + "]", match)
     chinese_punctuation_pattern = r"[：；！（）【】『』「」《》－‘“’”:,;!\(\)><\-]"
     text = re.sub(chinese_punctuation_pattern, ' ', text)
     # 使用正则表达式将多个连续的句号替换为一个句号
